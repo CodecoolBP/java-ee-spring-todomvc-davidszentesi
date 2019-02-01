f7d1614009598de67ca70157837d8b978e09c3fb
David Szentesi <szntsd@gmail.com>
2019-02-01 15:33:30 +0100

    add sample data

--- src/main/java/com/codecool/basictodolist/BasicTodoList.java
+++ src/main/java/com/codecool/basictodolist/BasicTodoList.java
@@ -1,5 +1,7 @@
 package com.codecool.basictodolist;
 
+import com.codecool.basictodolist.model.Todo;
+import com.codecool.basictodolist.model.TodoDao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BasicTodoList {
 
-//    private static final String SUCCESS = "{\"success\":true}";
-
     public static void main(String[] args) {
+        addSampleData();
         SpringApplication.run(BasicTodoList.class, args);
+    }
+
+    private static void addSampleData() {
+        TodoDao.add(Todo.create("first TODO item"));
+        TodoDao.add(Todo.create("second TODO item"));
+        TodoDao.add(Todo.create("third TODO item"));
     }
 
 }
